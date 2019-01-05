c48cef6c56d0b386ff62835c8e746f3c8dbded50
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-05 19:18:33 +0700

    Add files via upload

---
+++ PROGRAM C MENENTUKAN BILANGAN TERKECIL DARI 3 BILANGAN YANG DI INPUTKAN.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int minimum(int a,int b,int c);
+int main(void)
+{
+int a, b, c, min;
+printf("PROGRAM MENENTUKAN BILANGAN TERKECIL DARI 3 BILANGAN YANG DI INPUTKAN\n");
+printf("Masukan Bilangan Pertama : ");scanf("%d",&a);
+printf("Masukan Bilangan Kedua   : ");scanf("%d",&b);
+printf("Masukan Bilangan Ketiga  : ");scanf("%d",&c);
+min=minimum(a,b,c);
+ printf("Bilangan Terkecil Adalah : %d",min);
+ getch();
+}
+int minimum(int a,int b,int c)
+{
+    int main;
+    if(a>b)
+    else
+    if(c<min)
+    return (min);
+
+}
